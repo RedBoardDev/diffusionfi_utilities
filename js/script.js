d5e9c90c8d81ae55126b95bb22d2bbee31cf0f53
redboard <ott.thomas68@gmail.com>
2022-11-05 12:59:36 +0100

    (+) check what page is it

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const insertAfter = (newNode, referenceNode) => {
+    if (referenceNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+};
+
+function checkPageUrl(strInd, strRe) {
+    const urlIndex = window.location.href;
+    var urlIndex1 = 0;
+    var urlIndex2 = 0;
+    if (strInd != false)
+        urlIndex1 = urlIndex.indexOf(strInd);
+    if (strRe != false)
+        urlIndex2 = urlIndex.search(strRe);
+    if (urlIndex1 != -1 && urlIndex2 != -1) {
+        return (true);
+    } else
+        return (false);
+}
+
+function pageSwap() {
+    alert("SWAP PAGE");
+}
+
+function pageFarm() {
+    alert("FARM PAGE");
+}
+
+window.addEventListener('load', function () {
+    console.log("debug1")
+    if (checkPageUrl("swap", false))
+        pageSwap();
+    if (checkPageUrl("farm/", /[0-99]/g))
+        pageFarm();
+});
