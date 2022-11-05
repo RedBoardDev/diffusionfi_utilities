af7ec129a53f18e5c18bc0708a4dec5d2e633e46
redboard <ott.thomas68@gmail.com>
2022-11-05 14:36:07 +0100

    (-) move button function

--- js/buttons.js
+++ js/buttons.js
@@ -6,16 +6,3 @@ function create50PercentsButton(document) {
     button.className = "sc-7ovl44-40-50p";
     insertAfter(button, neartag);
 }
-
-function button50Percents(document) {
-    const buttonOnClick = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-5.sc-7ovl44-6.cmwkpY > div > div.sc-kkGfuU.sc-7ad97h-0.sc-7ad97h-4.TXsfB > button.sc-7ovl44-40-50p");
-    buttonOnClick.addEventListener("click", () => {
-        const countToken_doc = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-5.sc-7ovl44-6.cmwkpY > div > div.sc-kkGfuU.sc-7ad97h-0.sc-7ad97h-4.TXsfB > div");
-        const countToken = parseFloat(countToken_doc.textContent.split(" ")[1]);
-        console.log(countToken);
-        const fillInput = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-4.eYnRMv > input");
-        fillInput.value = countToken / 2;
-        const change = new InputEvent('change', { bubbles: true });
-        fillInput.dispatchEvent(change);
-    });
-};
--- js/script.js
+++ js/script.js
@@ -4,6 +4,28 @@ const insertAfter = (newNode, referenceNode) => {
     }
 };
 
+function create50PercentsButton(document) {
+    const neartag = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-5.sc-7ovl44-6.cmwkpY > div > div.sc-kkGfuU.sc-7ad97h-0.sc-7ad97h-4.TXsfB > div");
+    console.log("debug2", neartag);
+    const button = document.createElement('button');
+    button.innerHTML = '(50%)';
+    button.className = "sc-7ovl44-40-50p";
+    insertAfter(button, neartag);
+}
+
+function button50Percents(document) {
+    const buttonOnClick = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-5.sc-7ovl44-6.cmwkpY > div > div.sc-kkGfuU.sc-7ad97h-0.sc-7ad97h-4.TXsfB > button.sc-7ovl44-40-50p");
+    buttonOnClick.addEventListener("click", () => {
+        const countToken_doc = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-5.sc-7ovl44-6.cmwkpY > div > div.sc-kkGfuU.sc-7ad97h-0.sc-7ad97h-4.TXsfB > div");
+        const countToken = parseFloat(countToken_doc.textContent.split(" ")[1]);
+        console.log(countToken);
+        const fillInput = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-4.eYnRMv > input");
+        fillInput.value = countToken / 2;
+        const change = new InputEvent('change', { bubbles: true });
+        fillInput.dispatchEvent(change);
+    });
+};
+
 function checkPageUrl(strInd, strRe) {
     const urlIndex = window.location.href;
     var urlIndex1 = 0;
@@ -19,8 +41,9 @@ function checkPageUrl(strInd, strRe) {
 }
 
 function pageSwap() {
+    console.log("debug1");
     create50PercentsButton(document);
-    button50Percents(document);
+    // button50Percents(document)
 }
 
 function pageFarm() {
@@ -28,9 +51,16 @@ function pageFarm() {
 }
 
 window.addEventListener('load', function () {
-    console.log("debug1")
+    // create50PercentsButton(document)
+
+    // const buttonOnClick = document.querySelector("#swap-currency-input > div > div.sc-7ovl44-5.sc-7ovl44-6.cmwkpY > div > div.sc-kkGfuU.sc-7ad97h-0.sc-7ad97h-4.TXsfB > button.sc-7ovl44-40-50p");
+    // buttonOnClick.addEventListener("click", () => {
+    //     button50Percents(document)
+    // });
     if (checkPageUrl("swap", false))
         pageSwap();
     if (checkPageUrl("farm/", /[0-99]/g))
         pageFarm();
+
 });
+
